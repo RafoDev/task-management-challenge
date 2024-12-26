6754bc9a234edf996d04b05a4efe5131ca419102
RafoDev <rafael.david@ucsp.edu.pe>
2024-12-26 16:04:45 -0500

    feat: add svgr plugin to manage icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import svgr from "vite-plugin-svgr";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    svgr({
+      include: "**/*.svg?react",
+    }),
+  ],
+});
