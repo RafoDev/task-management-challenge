fdfd93fc4968f008a96de290beeaaddf18b2ce81
RafoDev <rafael.david@ucsp.edu.pe>
2024-12-26 18:25:39 -0500

    feat(top-navbar): add styles and interactivity for inputfield

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { TopNavbar } from "./features/navigation/top-navbar/top-navbar";
+import { useGetAllTasksQuery } from "./getTasks.generated";
 
 function App() {
+  const { data, loading } = useGetAllTasksQuery();
+
   return (
     <>
       <TopNavbar />
+      {loading ? (
+        <span>loading...</span>
+      ) : (
+        <ul>
+          {data?.tasks.map((task) => (
+            <li key={task.id}>
+              <h4>{task.name}</h4>
+              <span>{task.status}</span>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
--- src/features/navigation/top-navbar/top-navbar.tsx
+++ src/features/navigation/top-navbar/top-navbar.tsx
@@ -3,20 +3,39 @@ import NotificationIcon from "/src/assets/icons/notification.svg?react";
 import CleanIcon from "/src/assets/icons/clean.svg?react";
 import styles from "./top-navbar.module.scss";
 import { Profile } from "./components";
+import { useState } from "react";
 
 export const TopNavbar = () => {
+  const [showInput, setShowInput] = useState(false);
+
+  const onShowInput = () => {
+    setShowInput(!showInput);
+  };
+
   return (
     <nav className={styles.container}>
       <Profile />
-      <form className={styles.inputContainer}>
-        <input type="text" placeholder="Search" />
+      <form
+        className={`${styles.inputContainer} ${
+          showInput ? styles.inputContainerShow : ""
+        }`}
+      >
+        <div
+          className={styles.searchIconContainer}
+          onClick={() => onShowInput()}
+        >
+          <SearchIcon className={styles.icon} />
+        </div>
+        <input
+          type="text"
+          placeholder="Search"
+          className={`${styles.input} ${showInput ? styles.inputShow : ""}`}
+        />
         <div className={styles.cleanIconContainer}>
           <CleanIcon className={styles.icon} />
         </div>
       </form>
-      <div className={styles.searchIconContainer}>
-        <SearchIcon className={styles.icon} />
-      </div>
+
       <div className={styles.notificationIconContainer}>
         <NotificationIcon className={styles.icon} />
       </div>
