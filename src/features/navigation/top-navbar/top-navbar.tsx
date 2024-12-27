7c6a020c63bee2978e7b22436e16a6f991712332
RafoDev <rafael.david@ucsp.edu.pe>
2024-12-27 13:36:26 -0500

    feat(top-navbar): add responsive input styling for medium screens (≥768px)

--- src/features/navigation/top-navbar/top-navbar.tsx
+++ src/features/navigation/top-navbar/top-navbar.tsx
@@ -3,21 +3,45 @@ import NotificationIcon from "/src/assets/icons/notification.svg?react";
 import CleanIcon from "/src/assets/icons/clean.svg?react";
 import styles from "./top-navbar.module.scss";
 import { Profile } from "./components";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export const TopNavbar = () => {
   const [showInput, setShowInput] = useState(false);
+  const [animateInput, setAnimateInput] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const onShowInput = () => {
-    setShowInput(!showInput);
+    inputRef?.current?.focus();
+    if (showInput) {
+      setTimeout(() => setShowInput(false), 250);
+    } else {
+      setShowInput(true);
+      setAnimateInput(true);
+    }
+  };
+
+  useEffect(() => {
+    if (!showInput) setAnimateInput(false);
+  }, [showInput]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const clearInput = () => {
+    inputRef?.current?.focus();
+    setInputValue("");
   };
 
   return (
     <nav className={styles.container}>
-      <Profile />
+      <div className="profileContainer">
+        <Profile />
+      </div>
       <form
         className={`${styles.inputContainer} ${
-          showInput ? styles.inputContainerShow : ""
+          animateInput ? styles.inputContainerShow : ""
         }`}
       >
         <div
@@ -27,11 +51,23 @@ export const TopNavbar = () => {
           <SearchIcon className={styles.icon} />
         </div>
         <input
+          ref={inputRef}
           type="text"
           placeholder="Search"
-          className={`${styles.input} ${showInput ? styles.inputShow : ""}`}
+          className={`${styles.input} body-m-regular ${
+            animateInput ? styles.inputShow : ""
+          }`}
+          value={inputValue}
+          onChange={handleInputChange}
         />
-        <div className={styles.cleanIconContainer}>
+        <div
+          className={`${styles.cleanIconContainer} ${
+            inputValue.length > 0 && showInput
+              ? styles.cleanIconContainerShow
+              : ""
+          }`}
+          onClick={() => clearInput()}
+        >
           <CleanIcon className={styles.icon} />
         </div>
       </form>
