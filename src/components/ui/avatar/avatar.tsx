5d5f222fa96a95298f189433fb68c4578c568959
RafoDev <rafael.david@ucsp.edu.pe>
2024-12-30 09:28:14 -0500

    feat(avatar): add support for small and medium container sizes by props

--- src/components/ui/avatar/avatar.tsx
+++ src/components/ui/avatar/avatar.tsx
@@ -1,18 +1,25 @@
 import { User } from "../../../types";
 import styles from "./avatar.module.scss";
 
-type AvatarType = Pick<User, "id" | "fullName" | "avatar">;
+type Size = "s" | "m";
+type AvatarType = Pick<User, "id" | "fullName" | "avatar"> & {
+  size?: Size;
+};
+
+const styleMapping: Record<Size, string> = {
+  s: styles["container-s"],
+  m: styles["container-m"],
+};
 
 const placeholder = "/src/assets/images/user-placeholder.png";
 
-export const Avatar = ({ avatar, fullName }: AvatarType) => {
-  
+export const Avatar = ({ avatar, fullName, size = "s" }: AvatarType) => {
   const handleImageError = (event: React.SyntheticEvent<HTMLImageElement>) => {
     event.currentTarget.src = placeholder;
   };
 
   return (
-    <figure className={styles.container}>
+    <figure className={`${styles.container} ${styleMapping[size]}`}>
       <img
         src={avatar || placeholder}
         alt={fullName}
