f8f4e65b900ee0a1a43dbf934897bd830fe2b576
RafoDev <rafael.david@ucsp.edu.pe>
2024-12-30 12:54:32 -0500

    feat(cell): add styles and icons for header

--- src/features/tasks/table-view/components/cell/cell.tsx
+++ src/features/tasks/table-view/components/cell/cell.tsx
@@ -2,6 +2,9 @@ import { Status } from "../../../../../types";
 import { useGetTasksQuery } from "../../../getTasks.generated";
 import { Row } from "../row/row";
 import styles from "./cell.module.scss";
+import ArrowDownIcon from "/src/assets/icons/arrow-down.svg?react";
+import PlusIcon from "/src/assets/icons/plus.svg?react";
+import ThreeDots from "/src/assets/icons/three-dots.svg?react";
 
 type CellType = {
   title: string;
@@ -16,14 +19,34 @@ export const Cell = ({ title, status }: CellType) => {
 
   return (
     <section className={styles.container}>
-      <h2 className={`${styles.title} body-l-bold`}>
-        {title} ({data?.tasks.length})
-      </h2>
-      <section className={styles.taskContainer}>
+      <header className={styles.header}>
+        <div className={styles.titleContainer}>
+          <figure className={styles.iconContainer}>
+            <ArrowDownIcon className={styles.icon} />
+          </figure>
+
+          <h2 className={`${styles.title} body-l-bold`}>
+            {title}{" "}
+            <span className={`${styles.count} body-l-bold`}>
+              ({data?.tasks.length})
+            </span>
+          </h2>
+        </div>
+        <section className={styles.options}>
+          <figure className={styles.iconContainer}>
+            <PlusIcon className={styles.icon} />
+          </figure>
+          <figure className={styles.iconContainer}>
+            <ThreeDots className={styles.icon} />
+          </figure>
+        </section>
+      </header>
+
+      {/* <section className={styles.taskContainer}>
         {data?.tasks.map((task) => (
           <Row key={task.id} {...task} />
-        ))} 
-      </section>
+        ))}
+      </section> */}
     </section>
   );
 };
