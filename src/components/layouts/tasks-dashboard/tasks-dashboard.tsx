18f7b828ece58e796c3905edb4a8015f492c1940
RafoDev <rafael.david@ucsp.edu.pe>
2024-12-31 15:36:29 -0500

    feat(topbar): update button logic to toggle view modes instead of navigating routes

--- src/components/layouts/tasks-dashboard/tasks-dashboard.tsx
+++ src/components/layouts/tasks-dashboard/tasks-dashboard.tsx
@@ -3,14 +3,28 @@ import { Outlet } from "react-router-dom";
 import styles from "./tasks-dashboard.module.scss";
 import { Topbar } from "../../../features/tasks/topbar/topbar";
 import { SearchBar } from "../../../features/navigation/searchbar/searchbar";
+import { useState } from "react";
 
-export const TasksDashboard = () => {
+export type ViewModes = "kanban" | "table";
+
+type TasksDashboardType = {
+  defaultViewMode: ViewModes;
+};
+
+export const TasksDashboard = ({
+  defaultViewMode = "kanban",
+}: TasksDashboardType) => {
+  const [viewMode, setViewMode] = useState<ViewModes>(defaultViewMode);
+  const toggleViewMode = () => {
+    setViewMode((view) => (view === "kanban" ? "table" : "kanban"));
+  };
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <SearchBar />
       </header>
-      <Topbar />
+      <Topbar viewMode={viewMode} toggleViewMode={toggleViewMode} />
       <div className={styles.content}>
         <Outlet />
       </div>
--- src/features/tasks/topbar/topbar.tsx
+++ src/features/tasks/topbar/topbar.tsx
@@ -1,34 +1,44 @@
 import GridIcon from "/src/assets/icons/dashboard.svg?react";
 import ListIcon from "/src/assets/icons/list.svg?react";
-
-import { NavLink } from "react-router-dom";
 import styles from "./topbar.module.scss";
+import { ViewModes } from "../../../components/layouts/tasks-dashboard/tasks-dashboard";
 
-export const Topbar = () => {
+type TopbarType = {
+  viewMode: ViewModes;
+  toggleViewMode: () => void;
+};
+
+export const Topbar = ({ viewMode, toggleViewMode }: TopbarType) => {
   return (
     <nav className={styles.container}>
-      <NavLink
-        to="/table"
-        className={({ isActive }) =>
-          isActive ? `${styles.link} ${styles.linkActive}` : `${styles.link}`
+      <button
+        className={
+          viewMode === "table"
+            ? `${styles.button} ${styles.buttonActive}`
+            : `${styles.button}`
         }
+        onClick={() => toggleViewMode()}
+        disabled={viewMode === "table"}
       >
         <figure className={styles.iconContainer}>
           <ListIcon className={styles.icon} />
         </figure>
         <span className={`${styles.label} body-s-regular`}>Task</span>
-      </NavLink>
-      <NavLink
-        to="/kanban"
-        className={({ isActive }) =>
-          isActive ? `${styles.link} ${styles.linkActive}` : `${styles.link}`
+      </button>
+      <button
+        className={
+          viewMode === "kanban"
+            ? `${styles.button} ${styles.buttonActive}`
+            : `${styles.button}`
         }
+        onClick={() => toggleViewMode()}
+        disabled={viewMode === "kanban"}
       >
         <figure className={styles.iconContainer}>
           <GridIcon className={styles.icon} />
         </figure>
         <span className={`${styles.label} body-s-regular`}>Dashboard</span>
-      </NavLink>
+      </button>
     </nav>
   );
 };
