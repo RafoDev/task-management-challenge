1d17243737d21be7b231e9f74a7958e089b2f9f8
RafoDev <rafael.david@ucsp.edu.pe>
2025-01-02 17:31:37 -0500

    feat(create-task): implement basic form structure for task creation dialog

--- src/features/tasks/topbar/components/create-task/create-task.tsx
+++ src/features/tasks/topbar/components/create-task/create-task.tsx
@@ -1,9 +1,27 @@
 import { Dialog, useDialog } from "../../../../../components/ui/dialog/dialog";
 import PlusIcon from "/src/assets/icons/plus.svg?react";
 import styles from "./create-task.module.scss";
+import { PointEstimate, TaskTag } from "../../../../../types";
+import { useGetUsersQuery } from "../../../getUsers.generated";
+
+const pointsEstimate: { value: PointEstimate; label: string }[] = [
+  { value: PointEstimate.One, label: "1 Points" },
+  { value: PointEstimate.Two, label: "2 Points" },
+  { value: PointEstimate.Four, label: "4 Points" },
+  { value: PointEstimate.Eight, label: "8 Points" },
+];
+
+const tags: { value: TaskTag; label: string }[] = [
+  { value: TaskTag.Android, label: "Android" },
+  { value: TaskTag.Ios, label: "IOS" },
+  { value: TaskTag.NodeJs, label: "NodeJS" },
+  { value: TaskTag.Rails, label: "Rails" },
+  { value: TaskTag.React, label: "React" },
+];
 
 export const CreateTask = () => {
   const { closeDialog, isDialogOpen, openDialog } = useDialog();
+  const { data, loading } = useGetUsersQuery();
 
   return (
     <>
@@ -11,9 +29,7 @@ export const CreateTask = () => {
         className={`${styles.button} ${styles.createButton}`}
         onClick={() => openDialog()}
       >
-        <figure className={styles.iconContainer}>
-          <PlusIcon className={styles.icon} />
-        </figure>
+        <PlusIcon className={styles.icon} />
       </button>
 
       <Dialog
@@ -21,8 +37,34 @@ export const CreateTask = () => {
         onClose={closeDialog}
         actions={<button onClick={() => closeDialog()}>Close</button>}
       >
-        <form>
-          <input type="text" placeholder="Task Name" />
+        <form className="form">
+          <input type="text" placeholder="Task Name" className="name" />
+          <select name="estimate" id="estimate" className="points">
+            <option value="" className="option">
+              Estimate
+            </option>
+            {pointsEstimate.map((points) => (
+              <option value={points.label}>{points.label}</option>
+            ))}
+          </select>
+          <select name="assignee" id="assignee" className="assignee">
+            <option value="" className="option">
+              Assignee
+            </option>
+            {data?.users.map((user) => (
+              <option value={user.id} className="option">
+                {user.fullName}
+              </option>
+            ))}
+          </select>
+          <select name="tags" id="tags" className="tags">
+            {tags.map((tag) => (
+              <option value={tag.value} className="option">
+                {tag.label}
+              </option>
+            ))}
+          </select>
+          <input type="date" />
         </form>
       </Dialog>
     </>
